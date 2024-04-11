ce527c19968424b982f59c0a58946335d79314a2
cescriba <cescriba-2022278@kinal.edu.gt>
2024-04-11 13:20:28 -0600

    HRS-3 #comment linea de codigo excluida #time 19s

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-dbConnection().then(() => {
-  console.log("Database connected");
-});
-
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
